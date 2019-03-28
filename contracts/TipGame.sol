2a9c104ad4bbb2fd4fd5f0bf51b427f646c81b1f
Barna Rátki <barna.ratki@gmail.com>
2019-03-28 15:12:59 +0100

    Finish

--- contracts/TipGame.sol
+++ contracts/TipGame.sol
@@ -2,12 +2,12 @@ pragma solidity >=0.4.22 <0.6.0;
 
 contract TipGame {
 
-    string solutionHash;
+    bytes32 solutionHash;
     uint pricePool;
 
-    constructor(string solution) public
+    constructor(bytes32 solution) public
     {
-        solutionHash = solution;
+        solutionHash = (solution);
         pricePool = 0;
     }
 
@@ -15,14 +15,31 @@ contract TipGame {
     {
         pricePool += msg.value;
     }
+    
+    function stringToBytes32(string memory source) public pure returns (bytes32 result) {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+    
+        assembly {
+            result := mload(add(source, 32))
+        }
+    }
 
-    function makeATip(string guess) public view returns (bool)
+    function keccakGetter(string memory test) public view returns (bytes32)
     {
-        var hs = string(keccak256(bytes(guess)));
-        if (hs == solutionHash)
+        return (keccak256(bytes(test)));
+    }
+
+    function makeATip(string memory guess) public returns (bool)
+    {
+        bytes32 hs = (keccak256(bytes(guess)));
+        bytes32 sol = (solutionHash);
+        if (hs == sol)
         {
             //wins
-            msg.sender.send(pricePool);
+            msg.sender.transfer(pricePool);
             return true;
         }
         return false;
