4e2b3df3c17834f9d72be4c7d1676f03bf4f30fb
Barna Rátki <barna.ratki@gmail.com>
2019-03-30 14:56:07 +0100

    Updates

---
+++ precache-manifest.c7dc8f28f7bc5b30386a7370e8e86a1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b08b4db1196870282a4",
+    "url": "/ripit/static/js/runtime~main.1b08b4db.js"
+  },
+  {
+    "revision": "47e7aa75edecd7247775",
+    "url": "/ripit/static/js/main.47e7aa75.chunk.js"
+  },
+  {
+    "revision": "9e48ba234e068b3e883c",
+    "url": "/ripit/static/js/1.9e48ba23.chunk.js"
+  },
+  {
+    "revision": "47e7aa75edecd7247775",
+    "url": "/ripit/static/css/main.c508d55f.chunk.css"
+  },
+  {
+    "revision": "801cc9d5feb9d1e4d628ec7c3d8d7b24",
+    "url": "/ripit/index.html"
+  }
+];
